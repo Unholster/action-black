1f8405884a9d281f8972363b9597e359518f627f
Alexandre de Tocqueville <alex.detocqueville@gmail.com>
2019-10-29 15:33:01 -0300

    Trying PR idea

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,18 @@
-FROM python:3
+FROM python:3-slim
 
+LABEL "com.github.actions.name"="Black Code Formatter"
+LABEL "com.github.actions.description"="Format Python code using black"
+LABEL "com.github.actions.icon"="code"
+LABEL "com.github.actions.color"="yellow"
+
+LABEL "repository"="https://github.com/lgeiger/black-action"
+LABEL "homepage"="https://github.com/lgeiger/black-action"
+LABEL "maintainer"="Lukas Geiger <lukas.geiger94@gmail.com>"
+
+RUN apt-get update && apt-get install -y git 
+# RUN apt-get install -y jq
 RUN pip install black
 
 COPY entrypoint.sh /entrypoint.sh
-RUN chmod 777 /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,41 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-black --check .
-OK=$?
+# For some reason, $GITHUB_REF wasn't working before. This is a good alternative.
+# REF=$(jq -r ".ref" "$GITHUB_EVENT_PATH")
+# echo "Ref from JSON: $REF"
 
-if [ $OK -ne 0 ]
+if ! git status > /dev/null 2>&1
 then
-  black --diff .
+  echo "## Initializing git repo..."
+  git init
 fi
 
-exit $OK
+REMOTE_TOKEN_URL="https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+if ! git remote | grep "origin" > /dev/null 2>&1
+then 
+  echo "### Adding git remote..."
+  git remote add origin $REMOTE_TOKEN_URL
+  echo "### git fetch..."
+  git fetch
+fi
+
+git remote set-url --push origin $REMOTE_TOKEN_URL
+
+BRANCH="$GITHUB_HEAD_REF"
+echo "### Branch: $BRANCH"
+git checkout $BRANCH
+
+echo "## Login into git..."
+git config --global user.email "black_code_formatter@gmail.com"
+git config --global user.name "Black Code Formatter"
+
+echo "## Running Black Code Formatter"
+black $BLACK_ARGS
+
+echo "## Staging changes..."
+git add .
+echo "## Commiting files..."
+git commit -m "Black Automatically Formatted Code" || true
+echo "## Pushing to $BRANCH"
+git push -u origin $BRANCH
